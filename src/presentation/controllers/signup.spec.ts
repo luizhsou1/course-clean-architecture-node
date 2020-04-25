993ca180223836f43d449aee4978a8c29cf50771
Luiz Henrique <luizhsou1@gmail.com>
2020-04-25 20:08:45 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400) // Compara o ponteiro
     expect(httpResponse.body).toEqual(new Error('Missing param: name')) // Compara o conteúdo apenas
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400) // Compara o ponteiro
+    expect(httpResponse.body).toEqual(new Error('Missing param: email')) // Compara o conteúdo apenas
+  })
 })
