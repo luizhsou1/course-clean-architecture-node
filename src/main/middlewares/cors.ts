f729d94ac1380f6fe70a810077bafae1737f7fcf
Luiz <luizhsou1@gmail.com>
2020-05-04 10:47:58 -0300

    refactor: add return type middleware

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'; // json faz exatamente o que o body parser fazia, o express integrou essa biblioteca dentro dele
 
-export const cors = (req: Request, res: Response, next: NextFunction) => {
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('acess-control-allow-origin', '*');
   res.set('acess-control-allow-methods', '*');
   res.set('acess-control-allow-headers', '*');
