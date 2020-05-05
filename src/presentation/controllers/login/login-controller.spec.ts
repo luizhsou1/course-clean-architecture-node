7fb76b56c67c1e32c0734d854a5f2931be8db31a
Luiz <luizhsou1@gmail.com>
2020-05-05 09:58:01 -0300

    refactor: remove code duplication

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoginController } from './login-controller';
 import { ok, badRequest, serverError } from '../../helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '../../errors';
-import { EmailValidator } from '../../protocols';
+import { EmailValidator, HttpRequest } from '../../protocols';
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -11,6 +11,14 @@ const makeEmailValidator = (): EmailValidator => {
   }
   return new EmailValidatorStub();
 };
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password',
+  },
+});
+
 interface SutTypes {
   sut: LoginController;
   emailValidatorStub: EmailValidator;
@@ -51,26 +59,14 @@ describe('Login Controller', () => {
   test('Should return 400 if invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-      },
-    };
-    const httpResponse = await sut.handle(httpRequest);
+    const httpResponse = await sut.handle(makeFakeHttpRequest());
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')));
   });
 
   test('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut();
     const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-      },
-    };
-    await sut.handle(httpRequest);
+    await sut.handle(makeFakeHttpRequest());
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
 });
