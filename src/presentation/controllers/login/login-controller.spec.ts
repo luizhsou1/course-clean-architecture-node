36dbb23a7be8498ca8a459163fb8735372ee05f1
Luiz <luizhsou1@gmail.com>
2020-05-05 10:04:34 -0300

    test: ensure LoginController return 500 if EmailValidator throws

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -69,4 +69,13 @@ describe('Login Controller', () => {
     await sut.handle(makeFakeHttpRequest());
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const httpResponse = await sut.handle(makeFakeHttpRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
