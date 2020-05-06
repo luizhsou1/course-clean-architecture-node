57dc5cefcc346f9291b972481d80cf5523973bb2
Luiz <luizhsou1@gmail.com>
2020-05-05 23:22:03 -0300

    refactor: remove duplicated code

--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -3,13 +3,7 @@ import { LogErrorRepository } from '../../data/protocols/db/log/log-error-reposi
 
 //Usando Decorator para não ter que mexer nos meus controllers, incluindo a responsabilidade de logar possiveis erros
 export class LogControllerDecorator implements Controller {
-  private readonly controller: Controller;
-  private readonly logErrorRepository: LogErrorRepository;
-
-  constructor(controller: Controller, logErrorRepository: LogErrorRepository) {
-    this.controller = controller;
-    this.logErrorRepository = logErrorRepository;
-  }
+  constructor(private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest);
--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -2,12 +2,7 @@ import { Controller, HttpRequest, HttpResponse, Validation, Authentication } fro
 import { ok, badRequest, serverError, unauthorized } from '../../helpers/http/http-helper';
 
 export class LoginController implements Controller {
-  private readonly authentication: Authentication;
-  private readonly validation: Validation;
-  constructor(authentication: Authentication, validation: Validation) {
-    this.authentication = authentication;
-    this.validation = validation;
-  }
+  constructor(private readonly authentication: Authentication, private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
