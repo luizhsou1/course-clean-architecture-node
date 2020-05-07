48a08de02cc061e9d0cc045f291937b5cc9aa772
Luiz <luizhsou1@gmail.com>
2020-05-07 11:42:22 -0300

    test: ensure AddSurveyController calls AddSurvey of usecase folder with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,4 @@
-import { HttpRequest } from './add-survey-protocols';
+import { HttpRequest, AddSurvey, AddSurveyModel } from './add-survey-protocols';
 import { AddSurveyController } from './add-survey-controller';
 import { Validation } from '../../../protocols';
 import { badRequest } from '../../../helpers/http/http-helper';
@@ -24,17 +24,29 @@ const makeValidation = (): Validation => {
   return new ValidationStub();
 };
 
+const makeAddSurvey = (): AddSurvey => {
+  class AddSurveyStub implements AddSurvey {
+    async add(data: AddSurveyModel): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+  return new AddSurveyStub();
+};
+
 interface SutTypes {
   sut: AddSurveyController;
   validationStub: Validation;
+  addSurveyStub: AddSurvey;
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = makeValidation();
-  const sut = new AddSurveyController(validationStub);
+  const addSurveyStub = makeAddSurvey();
+  const sut = new AddSurveyController(validationStub, addSurveyStub);
   return {
     sut,
     validationStub,
+    addSurveyStub,
   };
 };
 
@@ -52,5 +64,13 @@ describe('Add Survey Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValue(new Error());
     const httpResponse = await sut.handle(makeFakeHttpRequest());
     expect(httpResponse).toEqual(badRequest(new Error()));
+  });
+
+  test('Should call AddSurvey with correct values', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyStub, 'add');
+    const httpRequest = makeFakeHttpRequest();
+    await sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
   });
 });
