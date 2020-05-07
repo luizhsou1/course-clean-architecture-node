23d5669ffed50b099a1c86ccad18ee2086ddd869
Luiz <luizhsou1@gmail.com>
2020-05-07 17:46:08 -0300

    refactor: rename to file

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -2,7 +2,7 @@ import { makeSignUpValidation } from './signup-validation-factory';
 import { SignUpController } from '../../../../presentation/controllers/login/signup/signup-controller';
 import { Controller } from '../../../../presentation/protocols';
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory';
-import { makeDbAddAccount } from '../../usecases/add-account/db-add-aaccount-factory';
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory';
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory';
 
 export const makeSignUpController = (): Controller => {
