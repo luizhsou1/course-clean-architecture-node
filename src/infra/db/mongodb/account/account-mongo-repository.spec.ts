c2f7bcc83d8f9c03f221c3a9516138d00a3ea0a4
Luiz <luizhsou1@gmail.com>
2020-05-07 20:41:43 -0300

    test: ensure AccountMongoRepository returns an account on loadByToken without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -72,4 +72,17 @@ describe('Account Mongo Repository', () => {
       expect(account.acessToken).toBe('any_token');
     });
   });
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role', async () => {
+      const sut = makeSut();
+      await accountCollection.insertOne({ ...makeFakeAddAccountModel(), accessToken: 'any_token' });
+      const account = await sut.loadByToken('any_token');
+      expect(account).toBeTruthy();
+      expect(account.id).toBeTruthy();
+      expect(account.name).toBe('any_name');
+      expect(account.email).toBe('any_email@mail.com');
+      expect(account.password).toBe('any_password');
+    });
+  });
 });
