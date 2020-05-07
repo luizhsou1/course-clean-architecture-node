0475567742c0968ae8dfe382b30b161094857cb8
Luiz <luizhsou1@gmail.com>
2020-05-07 17:41:33 -0300

    test: ensure JwtAdapter returns decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,10 +1,13 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
-import { rejects } from 'assert';
 
 jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return new Promise((resolve) => resolve('any_token'));
+  },
+
+  async verify(token: string): Promise<string> {
+    return new Promise((resolve) => resolve('any_value'));
   },
 }));
 
@@ -34,6 +37,21 @@ describe('Jwt Adapter', () => {
       });
       const promise = sut.encrypt('any_id');
       expect(promise).rejects.toThrow();
+    });
+  });
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const sut = makeSut();
+      const verifySpy = jest.spyOn(jwt, 'verify');
+      await sut.decrypt('any_token');
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret');
+    });
+
+    test('Should return a value on verify sucess', async () => {
+      const sut = makeSut();
+      const value = await sut.decrypt('any_token');
+      expect(value).toBe('any_value');
     });
   });
 });
